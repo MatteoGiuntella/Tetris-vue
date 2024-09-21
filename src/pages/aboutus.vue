90753627c3ba5decfaee7012fa06c32cdfa0e91b
Matteo Giuntella <145116327+MatteoGiuntella@users.noreply.github.com>
2024-09-21 13:19:04 +0200

    Initial commit

---
+++ src/pages/aboutus.vue
@@ -0,0 +1,23 @@
+<script>
+import {store} from '../store.js'
+export default {
+    data() {
+        return {
+            store
+        };
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+   <h1>about us</h1>
+   <span>ciao</span>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/aboutus.scss'
+
+</style>
